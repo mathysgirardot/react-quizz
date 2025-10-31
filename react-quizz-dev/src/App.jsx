db5d21c4f70efe095ea128c761d2c6c38cafb2fd
mathysgirardot <149168818+mathysgirardot@users.noreply.github.com>
2025-10-31 10:03:59 +0100

    Intégration premier composants
    J'ai créé les premières fonctions compostants pour pouvoir ensuite faire les bases du 2048.

--- react-quizz-dev/src/App.jsx
+++ react-quizz-dev/src/App.jsx
@@ -1,13 +1,25 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+function Header() {
+  return <h1>2048</h1>;
+}
+
+function GameBoard() {
+  return <p>Lorem</p>;
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Header/>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +28,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h2>Vite + React</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
