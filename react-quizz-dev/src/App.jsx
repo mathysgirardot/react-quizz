3cb776b815ad1882700ca5228062a8e9cd7ea338
mathysgirardot <149168818+mathysgirardot@users.noreply.github.com>
2025-11-02 12:20:54 +0100

    Remise à défault de la branch main

--- react-quizz-dev/src/App.jsx
+++ react-quizz-dev/src/App.jsx
@@ -4,26 +4,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Header() {
-  return <h1>2048</h1>;
-}
-
-function GameBoard() {
-  return <p>Lorem</p>;
-}
-
-function Card() {
-  return <p>Lorem</p>;
-}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Header/>
-      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
